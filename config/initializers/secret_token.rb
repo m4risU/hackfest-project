51360175e07e0fb51f5ff66ad93001c61797d49b
Mariusz Wyrozębski <mariuszwyrozebski@gmail.com>
2012-04-13 16:52:07 +0200

    initialize project with rails generated project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackfestProject::Application.config.secret_token = '80e62a224f8f372e5a8fdfc484c129250ddabf336fbe6d09473983f4c6739f542817a50670bfb99dff1f77c17f5b4393f1507b3a2feb8f4f0a68b57e62332121'
