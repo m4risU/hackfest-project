c0fc92fcc8a90909b421e1645f05498f29645207
Mariusz Wyrozębski <mariuszwyrozebski@gmail.com>
2012-04-15 11:29:33 +0200

    presentation layer

--- app/models/departure.rb
+++ app/models/departure.rb
@@ -3,5 +3,11 @@ class Departure < ActiveRecord::Base
 
   def to_s
     departing_at.strftime("%H:%M")
+    #Time.now.strftime("%H:%M") + "/" +departing_at.strftime("%H:%M")
+    #t1 = departing_at
+    #t2 = Time.now
+    #
+    #diff = t2 - t1     # diff in seconds
+
   end
 end
